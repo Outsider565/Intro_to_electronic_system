d6540f34528f4df0006461a83b9bee0be091b890
Jack_Wang <shaowenwang@icloud.com>
2020-06-15 10:42:15 +0800

    增加了初始速度接口，增加了初始积分项的接口来减少稳定时间 修复了计算积分项时，计算错误的问题

--- Car/CarCtrl.py
+++ Car/CarCtrl.py
@@ -2,9 +2,9 @@ import CarInfo
 import threading
 import numpy as np
 
-
 class CarCtrl:
-    def __init__(self, speed=100, diff=0, period=0.05, kp=100, ki=12, kd=1):
+    def __init__(self, speed=100, diff=0, period=0.05, kp=80, ki=0.95, kd=1.2, if_print=True, init_l_speed=100,
+                 init_r_speed=78, init_time=0.1, i_init_value=60):
         self.period = period
         self.basis = CarInfo.CarInfo(period)
         self.expected_diff = diff
@@ -13,6 +13,15 @@ class CarCtrl:
         self.kp = kp
         self.ki = ki
         self.kd = kd
+        self.p_diff = 0
+        self.i_diff = 0
+        self.d_diff = 0
+        self.init_l_speed = init_l_speed
+        self.init_r_speed = init_r_speed
+        self.init_time = init_time
+        self.i_init_value = i_init_value
+        self.raw_diff_list = []
+        self.if_print = if_print
         self.__end_flag = threading.Event()
 
     def set_expected_diff(self, val):
@@ -22,17 +31,23 @@ class CarCtrl:
         self.power = val
 
     def get_p_diff(self, t=None):
-        return (self.basis.get_r_round(t) - self.basis.get_l_round(t) - self.expected_diff) * self.kp
+        ans = (self.basis.get_r_round(t) - self.basis.get_l_round(t) - self.expected_diff)
+        if t is None:
+            self.raw_diff_list.append(ans)
+        self.p_diff = ans * self.kp
+        return ans * self.kp
 
     def get_i_diff(self):
         self.basis.update_speed_and_round()
-        if len(self.basis.r_round) == 0:
-            return - self.expected_diff
+        if len(self.raw_diff_list) == 0:
+            return 0  # 此时默认小车按正常方向行走
         else:
-            return ((np.array(self.basis.r_round).sum() - np.array(
-                self.basis.l_round).sum()) / (len(self.basis.r_round)) - self.expected_diff) * self.ki
+            self.i_diff = np.array(self.raw_diff_list).sum() * self.ki
+            return np.array(self.raw_diff_list).sum() * self.ki
 
     def get_d_diff(self):
+        self.d_diff = (self.get_p_diff(self.basis.get_time()) - self.get_p_diff(
+            self.basis.get_time() - 1.5 * self.period)) * self.kd
         return (self.get_p_diff(self.basis.get_time()) - self.get_p_diff(
             self.basis.get_time() - 1.5 * self.period)) * self.kd
 
@@ -46,17 +61,28 @@ class CarCtrl:
         return min(self.basis.get_l_round(), self.basis.get_r_round())
 
     def __run(self):
+        """
+        会先在无指引的情况下运行self.period s，并且给raw然后再进入PID调控
+        :return:
+        """
+        self.raw_diff_list.append(self.i_init_value)
         while not self.__end_flag.isSet():
             try:
-                self.basis.set_left_power(max(self.power + min(self.get_diff(), 0), 0))
-                self.basis.set_right_power(max(self.power - max(self.get_diff(), 0), 0))
-                #self.print_info()
-                self.basis.stay(self.period)
+                if self.get_time() <= self.init_time:
+                    self.basis.set_left_power(self.init_l_speed)
+                    self.basis.set_right_power(self.init_r_speed)
+                    self.basis.stay(self.period)
+                else:
+                    self.basis.set_left_power(max(self.power + min(self.get_diff(), 0), 0))
+                    self.basis.set_right_power(max(self.power - max(self.get_diff(), 0), 0))
+                    if self.if_print:
+                        self.print_info()
+                    self.basis.stay(self.period)
             except Exception as e:
                 self.basis.set_left_power(100)
-                self.basis.set_right_power(100)
-                print("Run into exception: "+str(e))
-                #self.print_info()
+                self.basis.set_right_power(60)
+                print("Run into exception: " + str(e))
+                self.print_info()
                 self.basis.stay(self.period)
 
     def start(self):
@@ -72,12 +98,12 @@ class CarCtrl:
             self.basis.get_r_round()))
         print("l_power:" + '{:.1f}'.format(self.basis.get_l_power()) + "\tr_power: " + '{:.1f}'.format(
             self.basis.get_r_power()))
-        print("p: " + "{:.2f}".format(self.get_p_diff()) + "\ti: " + "{:.2f}".format(
-            self.get_i_diff()) + "\td: " + "{:.2f}".format(self.get_d_diff()))
+        print("p: " + "{:.2f}".format(self.p_diff) + "\ti: " + "{:.2f}".format(
+            self.i_diff) + "\td: " + "{:.2f}".format(self.d_diff))
         print("dist: ", self.get_distance())
 
     def get_distance(self, index=0):
-        return self.basis.get_distance(index)
+        return self.basis.get_round(index)
 
     @staticmethod
     def stay(t):
@@ -89,11 +115,13 @@ class CarCtrl:
 
 if __name__ == '__main__':
     try:
-        c = CarCtrl()
+        c = CarCtrl(i_init_value=40)
         c.start()
         c.set_power(100)
-        c.set_expected_diff(0)
-        c.stay(5)
+        c.set_expected_diff(-0.15)
+        c.stay(3)
+        # c.set_expected_diff(0.1)
+        # c.stay(3)
         # c.set_r_mode()
         # c.stay(3)
         # c.set_expected_diff()
